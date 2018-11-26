13d756e0c1ca11f85cd731c62259e8f7ec46e75f
Marek Bleschke <marek.bleschke@allegro.pl>
2018-11-26 16:38:19 +0100

    push env variables to config files

--- docker_ng/docker-entrypoint.sh
+++ docker_ng/docker-entrypoint.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
-set -eux
+set -e
+
+db_env_variables=(
+    DATABASE_NAME
+    DATABASE_USER
+    DATABASE_PASSWORD
+    DATABASE_HOST
+    DATABASE_PORT
+    DATABASE_ENGINE
+)
+db_conf_path="/etc/ralph/conf.d/database.conf"
+db_conf_path="/tmp/docker_ng/database.conf"
+
+redis_env_variables=(
+    REDIS_HOST
+    REDIS_PORT
+    REDIS_DB
+    REDIS_PASSWORD
+)
+redis_conf_path="/etc/ralph/conf.d/redis.conf"
+redis_conf_path="/tmp/docker_ng/redis.conf"
+
+function push_env_vars_to_config() {
+    local conf_path=$1
+    shift
+    local env_variables=("$@")
+    for env_var in "${env_variables[@]}"
+    do
+        if [[ ! -z "${!env_var}" ]]; then
+            sed -ri "s/(${env_var} ?= ?).*/\1${!env_var}/" ${conf_path}
+        fi
+    done
+}
+
+push_env_vars_to_config "$db_conf_path" "${db_env_variables[@]}"
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 #cd $DIR
