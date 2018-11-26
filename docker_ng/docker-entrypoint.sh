4771f65f0faab5f25c47e0672748dcde43f3f0a7
Maciej Skorczewski <maciej.skorczewski@allegro.pl>
2018-11-26 14:55:29 +0100

    Editted entrypoint

--- docker_ng/docker-entrypoint.sh
+++ docker_ng/docker-entrypoint.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-set -e
-
-# wait for db init
-sleep 15
+set -eux
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd $DIR
+#cd $DIR
 
-./upgrade.sh
+export DEBIAN_FRONTEND=noninteractive
 
 # pass the error (ex. when user already exist);
 # by muting the error, this script could be use to upgrade data container too
 $RALPH_EXEC createsuperuser --noinput --username ralph --email ralph@allegro.pl || true
 python3 $RALPH_DIR/docker_ng/createsuperuser.py
 
-set -e
-cd $RALPH_DIR
-
-make docs
+#cd $RALPH_DIR
 
 $RALPH_EXEC migrate --noinput
+$RALPH_EXEC collectstatic --noinput
+$RALPH_EXEC sitetree_resync_apps
 
-make menu
+
+
