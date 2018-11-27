fa2368aafcdf042cadbdc58f8508e6e2784c3992
Marek Bleschke <marek.bleschke@allegro.pl>
2018-11-27 15:58:09 +0100

    dockerfile

--- docker_ng/start-ralph.sh
+++ docker_ng/start-ralph.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-/opt/ralph/ralph-core/bin/gunicorn -c "${RALPH_LOCAL_DIR}/gunicorn.ini" ralph.wsgi
+/opt/ralph/ralph-core/bin/gunicorn --daemon -c "${RALPH_LOCAL_DIR}/gunicorn.ini" ralph.wsgi
+
+while [ ! -f /var/log/ralph/ralph.log ]
+do
+    sleep 1
+done
+
+tail -f /var/log/ralph/ralph.log
