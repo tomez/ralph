79b632387e5312695e70b0b78c54d5b76d8e1200
Roman Prykhodchenko <roman.prykhodchenko@allegro.pl>
2018-11-27 16:38:05 +0100

    Build package in Docker

--- packaging/build-package.sh
+++ packaging/build-package.sh
@@ -6,18 +6,6 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export DEB_DH_SHLIBDEPS_ARGS_ALL="--dpkg-shlibdeps-params=--ignore-missing-info"
 SRC_DIR="$DIR/.."
 
-function install_dh_virtualenv {
-	# in ubuntu 14.04 provided by system - just for another systems
-	# compile and install dh_virtualenv in travis sandbox - we need sudo here
-	sudo apt-get update
-	sudo apt-get install -y devscripts python-virtualenv git equivs
-	git clone https://github.com/spotify/dh-virtualenv.git
-	cd dh-virtualenv
-	sudo mk-build-deps -ri
-	dpkg-buildpackage -us -uc -b
-	sudo dpkg -i ../dh-virtualenv_*.deb
-}
-
 function build_package {
 	cd $SRC_DIR
 	if [ ! -z "$TRAVIS" ]; then
