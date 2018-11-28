d41e9976d4d66134383742bceaee75c3f97705bb
Marek Bleschke <marek.bleschke@allegro.pl>
2018-11-28 13:26:44 +0100

    better logs for docker compose

--- docker_ng/provision/start-ralph.sh
+++ docker_ng/provision/start-ralph.sh
@@ -2,9 +2,9 @@
 
 /opt/ralph/ralph-core/bin/gunicorn --daemon -c "${RALPH_LOCAL_DIR}/gunicorn.ini" ralph.wsgi
 
-while [ ! -f /var/log/ralph/ralph.log ]
+while [ ! -f /var/log/ralph/ralph.log ] || [ ! -f /var/log/ralph/gunicorn.error.log ]
 do
     sleep 1
 done
 
-tail -f /var/log/ralph/ralph.log
+tail -f /var/log/ralph/ralph.log /var/log/ralph/gunicorn.error.log
